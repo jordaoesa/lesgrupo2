c68125bf092cba812a714230b32215fa4b33edc9
jeremias.serafim.araujo <jeremias.serafim.araujo@b24eb590-6751-b4c3-0875-7fcc60168bca>
2013-02-27 05:20:21 +0000

    fachada

---
+++ trunk/FitnessManagement/src/br/edu/ufcg/fachada/ExercicioFachada.java
@@ -0,0 +1,48 @@
+package br.edu.ufcg.fachada;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import br.edu.ufcg.aluno.Dados;
+import br.edu.ufcg.exercicio.Exercicio;
+import br.edu.ufcg.sgbd.DB;
+
+public class ExercicioFachada {
+
+	private static final String TABLE_NAME = "TABLE_EXERCICIOS";
+	private DB db;
+	
+	public ExercicioFachada(DB db) {
+		this.db = db;
+	}
+
+	public void adicionaExercicio(Integer id, String nomeExercicio){
+		ContentValues values = new ContentValues();
+		values.put("id_exercicio", id);
+		values.put("nomeExercicio", nomeExercicio);
+		db.insertValues(TABLE_NAME, values);
+	}
+	
+	public String getDadosExercicio(Integer id) {
+		Cursor cursor = db.getReadableDatabase().query(TABLE_NAME, null, "id_exercicio = " + id, null, null, null, null);
+		String s = null;
+		try{
+			while(cursor.moveToNext()){
+				 s ="";
+				s = s+ cursor.getInt(0) +  cursor.getInt(1);
+				
+			}
+		}catch(Exception e){
+			e.getStackTrace();
+		}finally{
+			cursor.close();
+		}
+		return s;
+	}
+	
+}
