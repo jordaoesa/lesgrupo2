013840e682c6bc9ab2bd4cecb0f361905ff7604e
jordaotalk <jordaotalk@b24eb590-6751-b4c3-0875-7fcc60168bca>
2013-02-28 16:58:43 +0000

    activity aluno

--- trunk/FitnessManagement/src/br/edu/ufcg/fitnessmanagement/MainActivity.java
+++ trunk/FitnessManagement/src/br/edu/ufcg/fitnessmanagement/MainActivity.java
@@ -52,7 +52,8 @@ public class MainActivity extends Activity {
 		bCadastrar.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				menuCadastrar();
+				Intent intentCadastrarAluno = new Intent(getApplicationContext(), CadastrarAlunoActivity.class);
+				startActivity(intentCadastrarAluno);
 			}
 		});
 		bGerenciar.setOnClickListener(new OnClickListener() {
@@ -65,45 +66,6 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				inserirUsuarioNoBanco();
-			}
-		});
-		
-	}
-	
-	private void menuCadastrar() {
-		setContentView(R.layout.cadastrar);
-		
-		Button cadastrar = (Button) findViewById(R.id.buttonCadastrarMenuCadastrar);
-		cadastrar.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				
-				String nome = ((EditText) findViewById(R.id.editTextNomeMenuCadastrar)).getText().toString();
-				String endereco = ((EditText) findViewById(R.id.editTextEnderecoMenuCadastrar)).getText().toString();
-				String telefone = ((EditText) findViewById(R.id.editTextTelefoneMenuCadastrar)).getText().toString();
-				Integer idade = Integer.parseInt(((EditText) findViewById(R.id.editTextIdadeMenuCadastrar)).getText().toString());
-				final RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroupSexoMenuCadastrar);
-				Aluno aluno = null;
-				
-				
-				int sexo = radioGroup.getCheckedRadioButtonId();
-				if(sexo == R.id.radioButtonSexoMasMenuCadastrar){
-					aluno = new Aluno(nome, endereco, "Masculino", telefone, idade);
-				}else if(sexo == R.id.radioButtonSexoFemMenuCadastrar){
-					aluno = new Aluno(nome, endereco, "Feminino", telefone, idade);
-				}
-				
-				alunoFachada.adicionarAluno(aluno);
-				Toast.makeText(getApplicationContext(), "Cadastrado com Sucesso", Toast.LENGTH_SHORT).show();
-				menuInicial();
-			}
-		});
-		
-		Button voltar = (Button) findViewById(R.id.buttonVoltarMenuCadastrar);
-		voltar.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View arg0) {
-				menuInicial();
 			}
 		});
 		
