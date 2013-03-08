5c44145d04879a8e27a077b8b5e08e658c7dbd1a
jordaotalk <jordaotalk@b24eb590-6751-b4c3-0875-7fcc60168bca>
2013-03-08 14:50:43 +0000

    consertado bug no cadastrar exercicio/maquina/grupo muscular OK botão voltar OK bug ao cadastrar atividade com peso inválido OK

--- FitnessManagement/src/br/edu/ufcg/fitnessmanagement/CriarExercicioMaquinaGMuscularActivity.java
+++ FitnessManagement/src/br/edu/ufcg/fitnessmanagement/CriarExercicioMaquinaGMuscularActivity.java
@@ -6,9 +6,12 @@ import br.edu.ufcg.fachada.MaquinaFachada;
 import br.edu.ufcg.util.FitnessManagementSingleton;
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class CriarExercicioMaquinaGMuscularActivity extends Activity {
@@ -21,72 +24,61 @@ public class CriarExercicioMaquinaGMuscularActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_criar_exercicio_maquina_gmuscular_bd);
-		
+		setTitle("Cadastrar Exercícios");
 		
 		exercicioFachada = FitnessManagementSingleton.getExercicioFachadaInstance();
 		maquinaFachada =  FitnessManagementSingleton.getMaquinaFachadaInstance();
 		grupoMuscularFachada =  FitnessManagementSingleton.getGrupoMuscularFachadaInstance();
-//		
 		
 		menuCriarExercicioMaquinaGMuscular();
 	}
-	
-	
-	
-	
+
 	private void menuCriarExercicioMaquinaGMuscular() {
 		
-		Button botaoCriarExercicio = (Button) findViewById(R.id.botaoCriarExercicio);
-		Button botaoCriarMaquina = (Button)findViewById(R.id.botaoCriarMaquina);
-		Button botaoCriarGMuscular =(Button)findViewById(R.id.botaoCriarGrupoMuscular);
-//		
+		Button botaoCadastrar = (Button) findViewById(R.id.buttonCadastrarExercicioMaquinaMusculo);
+		Button botaoVoltar = (Button) findViewById(R.id.buttonVoltarCriarExercicioMaquinaMusculo);
+		
 		final TextView textViewExercicio = (TextView) findViewById(R.id.nomeExercicio);
 		final TextView textViewMaquina = (TextView) findViewById(R.id.nomeMaquina);
 		final TextView textViewGMuscular = (TextView) findViewById(R.id.nomeGrupoMuscular);
-//		
-		
-		botaoCriarExercicio.setOnClickListener(new View.OnClickListener() {
+
+		botaoCadastrar.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				String nomeExercicio = textViewExercicio.getText().toString();
 				
-				exercicioFachada.adicionaExercicio(nomeExercicio);
+				Editable nomeExercicio = (Editable) textViewExercicio.getText();
+				Editable nomeMaquina = (Editable) textViewMaquina.getText();
+				Editable nomeGrupoMuscular = (Editable) textViewGMuscular.getText();
+				
+				if(nomeExercicio == null || nomeExercicio.toString().equals("")){
+					Toast.makeText(getApplicationContext(), "Exercício Inválido", Toast.LENGTH_SHORT).show();
+				}else if(nomeMaquina == null || nomeMaquina.toString().equals("")){
+					Toast.makeText(getApplicationContext(), "Máquina Inválida", Toast.LENGTH_SHORT).show();
+				}else if(nomeGrupoMuscular == null || nomeGrupoMuscular.toString().equals("")){
+					Toast.makeText(getApplicationContext(), "Grupo Muscular Inválido", Toast.LENGTH_SHORT).show();
+				}else{
+					exercicioFachada.adicionaExercicio(nomeExercicio.toString());
+					maquinaFachada.adicionaMaquina(nomeMaquina.toString());
+					grupoMuscularFachada.adicionaGrupoMuscular(nomeGrupoMuscular.toString());
+					
+					textViewExercicio.setText("");
+					textViewMaquina.setText("");
+					textViewGMuscular.setText("");
+					
+					Toast.makeText(getApplicationContext(), "Sucesso", Toast.LENGTH_SHORT).show();
+				}
 				
 			}
 		});
 		
-		//adicionar maquina
-		botaoCriarMaquina.setOnClickListener(new View.OnClickListener() {
-			
+		botaoVoltar.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				String nomeMaquina = textViewMaquina.getText().toString();
-				maquinaFachada.adicionaMaquina(nomeMaquina);
+				finish();
 			}
 		});
-		
-		//criar grupo muscular
-		botaoCriarGMuscular.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				String nomeGrupoMuscular = textViewGMuscular.getText().toString();
-				grupoMuscularFachada.adicionaGrupoMuscular(nomeGrupoMuscular);
-			}
-		});
-		
-		
 		
 	}
-	
-	
-	
-	
-	
-	
-	
-
-	
 	
 }
