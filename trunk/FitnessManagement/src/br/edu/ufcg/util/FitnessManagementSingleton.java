d98f6e9b75f4e9c4d66ed4fbe10a3eb40288064c
yurisnm <yurisnm@b24eb590-6751-b4c3-0875-7fcc60168bca>
2013-04-05 05:04:05 +0000

    trash deleted

--- trunk/FitnessManagement/src/br/edu/ufcg/util/FitnessManagementSingleton.java
+++ trunk/FitnessManagement/src/br/edu/ufcg/util/FitnessManagementSingleton.java
@@ -6,15 +6,9 @@ import android.content.Context;
 import android.os.Environment;
 import br.edu.ufcg.fachada.AgendamentoFachada;
 import br.edu.ufcg.fachada.AlunoFachada;
-import br.edu.ufcg.fachada.AtividadeFachada;
 import br.edu.ufcg.fachada.DadosFachada;
-import br.edu.ufcg.fachada.ExercicioFachada;
-import br.edu.ufcg.fachada.FichaFachada;
-import br.edu.ufcg.fachada.GrupoMuscularFachada;
 import br.edu.ufcg.fachada.ImagemFachada;
-import br.edu.ufcg.fachada.MaquinaFachada;
 import br.edu.ufcg.fachada.FinancasFachada;
-import br.edu.ufcg.fachada.TreinoFachada;
 import br.edu.ufcg.sgbd.DB;
 
 public class FitnessManagementSingleton {
@@ -29,12 +23,7 @@ public class FitnessManagementSingleton {
 	private static DadosFachada dadosFachada;
 	private static FinancasFachada financasFachada;
 	private static ImagemFachada imagemFachada;
-	private static ExercicioFachada exercicioFachada;
-	private static MaquinaFachada maquinaFachada;
-	private static GrupoMuscularFachada grupoMuscularFachada;
-	private static AtividadeFachada atividadeFachada;
-	private static FichaFachada fichaFachada;
-	private static TreinoFachada treinoFachada;
+
 	private static AgendamentoFachada agendamentoFachada;
 	
 	public static void setContext(Context context){
@@ -62,24 +51,7 @@ public class FitnessManagementSingleton {
 		return dadosFachada;
 	}
 	
-	public static ExercicioFachada getExercicioFachadaInstance(){
-		if(exercicioFachada == null){
-			exercicioFachada = new ExercicioFachada(db);
-		}
-		return exercicioFachada;
-	}
-	public static MaquinaFachada getMaquinaFachadaInstance(){
-		if(maquinaFachada == null){
-			maquinaFachada = new MaquinaFachada(db);
-		}
-		return maquinaFachada;
-	}
-	public static GrupoMuscularFachada getGrupoMuscularFachadaInstance(){
-		if(grupoMuscularFachada == null){
-			grupoMuscularFachada = new GrupoMuscularFachada(db);
-		}
-		return grupoMuscularFachada;
-	}
+
 	
 	public static FinancasFachada getFinancasFachadaInstance(){
 		if(financasFachada == null){
@@ -101,20 +73,6 @@ public class FitnessManagementSingleton {
 //	}
 	
 	
-	public static FichaFachada getFichaFachadaInstance(){
-		if(fichaFachada == null){
-			fichaFachada = new FichaFachada(db);
-		}
-		return fichaFachada;
-	}
-	
-	public static TreinoFachada getTreinoFachadaInstance(){
-		if(treinoFachada == null){
-			treinoFachada = new TreinoFachada(db);
-		}
-		return treinoFachada;
-	}
-
 	public static ImagemFachada getImagemFachadaInstance() {
 		if(imagemFachada == null){
 			imagemFachada = new ImagemFachada(db);
