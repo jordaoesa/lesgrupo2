ad5e3a49e953de752b32c0764d4cb084c9c71924
jordaotalk <jordaotalk@b24eb590-6751-b4c3-0875-7fcc60168bca>
2013-04-10 06:07:55 +0000

    mudanças nomes de activities / versão android

--- trunk/FitnessManagement/src/br/edu/ufcg/fitnessmanagement/AvaliacaoFisicaActivity.java
+++ trunk/FitnessManagement/src/br/edu/ufcg/fitnessmanagement/AvaliacaoFisicaActivity.java
@@ -21,7 +21,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class CadastrarDadosActivity extends Activity {
+public class AvaliacaoFisicaActivity extends Activity {
 
 	private DadosFachada dadosFachada;
 	private Integer idAluno;
@@ -29,7 +29,7 @@ public class CadastrarDadosActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_cadastrar_dados);
+		setContentView(R.layout.activity_avaliacao_fisica);
 		setTitle("Atualizar Dados");
 		
 		dadosFachada = FitnessManagementSingleton.getDadosFachadaInstance();
@@ -142,7 +142,7 @@ public class CadastrarDadosActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_cadastrar_dados, menu);
+		getMenuInflater().inflate(R.menu.activity_avaliacao_fisica, menu);
 		return true;
 	}
 
