ae62a3ffe0dd19b82af23b34fd4fabf5efe8e65a
irvile.turin <irvile.turin@gmail.com>
2013-04-30 07:03:29 +0000

    fix tela grafico de metas

--- FitnessManagement/src/br/edu/ufcg/fitnessmanagement/VisualizaGraficoMetasActivity.java
+++ FitnessManagement/src/br/edu/ufcg/fitnessmanagement/VisualizaGraficoMetasActivity.java
@@ -3,6 +3,7 @@ package br.edu.ufcg.fitnessmanagement;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import br.edu.ufcg.fachada.MetasFachada;
 import br.edu.ufcg.grafico.GraficoDeLinha;
 import br.edu.ufcg.util.FitnessManagementSingleton;
@@ -15,6 +16,24 @@ public class VisualizaGraficoMetasActivity extends Activity {
 		int id_aluno = getIntent().getIntExtra("id_aluno", -1);
 		MetasFachada metaFachada = FitnessManagementSingleton.getMetaFachadaInstance();
 		Intent intent = grafico.getGraficoMetas(this, metaFachada.getMetaWeightLoss(id_aluno), id_aluno);
+        intent.setAction(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
 		startActivity(intent);
 	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+	    if ((keyCode == KeyEvent.KEYCODE_BACK)) {
+	    	System.out.println("OPAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+	    	finish();
+	    	return true;
+	    }
+	    return false;
+	}
+	@Override
+    public void onBackPressed() {
+        super.onBackPressed();   
+        //start activity A here
+
+    }
 }
